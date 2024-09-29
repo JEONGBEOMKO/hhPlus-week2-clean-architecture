91f403fa94c7ca38ab3f8305494cd44766d1e9fa
JEONGBEOM KO <beom9807@gmail.com>
2024-09-29 17:45:47 +0900

    first commit

---
+++ hhplus-week2-clean-architecture/src/test/java/com/hhplus/week2cleanarchitecture/HhplusWeek2CleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.week2cleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusWeek2CleanArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
